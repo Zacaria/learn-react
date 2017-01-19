68fce408c7eb941f5679c675a1a6efaa83c94fb4
Zacaria <zchtatar@excilys.com>
2017-01-19 10:12:55 +0100

    [UP] : bump version

---
+++ server/doc/api_data.js
@@ -0,0 +1,13 @@
+define({ "api": [
+  {
+    "type": "get",
+    "url": "/",
+    "title": "Flagz Root",
+    "name": "Root",
+    "description": "<p>The rocking flagz api</p>",
+    "group": "API",
+    "version": "0.0.0",
+    "filename": "server/src/router/index.js",
+    "groupTitle": "API"
+  }
+] });
