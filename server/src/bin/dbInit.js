fbc7d7589a9af8855b121bfc931704a397116837
Zacaria <zc@42.am>
2017-01-20 11:53:22 +0100

    [ADD] : Auto create tables

--- server/src/bin/dbInit.js
+++ server/src/bin/dbInit.js
@@ -1,15 +1,45 @@
 import config from 'config';
-const r = require('rethinkdbdash')();
+const r = require('rethinkdbdash')(config.rethinkdb);
 
-const init = () =>
+/**
+ * Creates db if it does not exist
+ * Uses env param to know which db to use
+ */
+const createDbIfMissing = (dbName) =>
     new Promise((resolve, reject) => {
         r.dbList()
-            .contains(config.rethinkdb.db)
-            .do((containsDb) =>
-                // Creates db if it does not exist
-                r.branch(containsDb, {created: 0}, r.dbCreate(config.rethinkdb.db))
-            )
+            .contains(dbName)
+            .do((containsDb) => r.branch(containsDb, {created: 0}, r.dbCreate(dbName)))
             .run()
+            .then(resolve)
+            .catch(reject);
+    });
+
+/**
+ * Creates tables if they do not exist
+ */
+const createTables = (tables) =>
+    Promise.all(tables
+        .map((table) =>
+            new Promise((resolve, reject) => {
+                r.tableList()
+                    .contains(table)
+                    .do((containsTable) => r.branch(containsTable, {created: 0}, r.tableCreate(table)))
+                    .run()
+                    .then(resolve)
+                    .catch(reject);
+            })
+        )
+    );
+
+
+/**
+ * Init rethinkDb database
+ */
+const init = () =>
+    new Promise((resolve, reject) => {
+        createDbIfMissing(config.rethinkdb.db)
+            .then(() => createTables(config.rethinkdb.tables))
             .then(resolve)
             .catch(reject);
     });
