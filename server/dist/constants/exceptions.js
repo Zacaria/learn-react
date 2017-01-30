d4815937e02172c31428158f7634788686e3a03e
Zacaria <zc@42.am>
2017-01-30 11:01:24 +0100

    [DEL] : dist files

--- server/dist/constants/exceptions.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-/**
- * Those messages are the one in res.json.message
- */
-var PATH_NOT_FOUND = exports.PATH_NOT_FOUND = 'Path not Found';
